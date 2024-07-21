55c37dbd127f153df063f536275dd7d92784483e
D.Yogesh <yogeshone678@gmail.com>
2024-07-22 04:29:14 +0530

    feat(client): add swapped box
    Signed-off-by: D.Yogesh <yogeshone678@gmail.com>

--- client/src/components/SwapBox.jsx
+++ client/src/components/SwapBox.jsx
@@ -6,6 +6,7 @@ const SwapComponent = ({ isConnected, onConnectClick }) => {
     { token: "DAI", amount: "" },
     { token: "BTC", amount: "" },
   ]);
+  const [tokenOut, setTokenOut] = useState("USDC");
 
   const handleTokenChange = (value, index) => {
     const newTokens = [...tokensIn];
@@ -21,9 +22,9 @@ const SwapComponent = ({ isConnected, onConnectClick }) => {
 
   return (
     <div>
-      <div className="max-w-md mx-auto mt-10 p-4 bg-gray-800 text-white rounded-lg shadow-xl">
+      <div className="max-w-md mx-auto mt-10 py-5 px-7 bg-gray-800 text-white rounded-lg shadow-xl">
         {tokensIn.map((token, index) => (
-          <div key={index} className="mb-4">
+          <div key={index} className="mb-4 flex flex-col items-start">
             <label className="block text-sm font-medium text-gray-400 mb-2">
               From (Token {index + 1}):
             </label>
@@ -47,7 +48,20 @@ const SwapComponent = ({ isConnected, onConnectClick }) => {
         ))}
       </div>
 
-      <div className="max-w-md mx-auto mt-5 p-4 flex flex-col items-center">
+      <div className="max-w-md mx-auto mt-5 py-5 px-7 bg-gray-800 text-white rounded-lg shadow-xl">
+        <label className="block text-sm font-medium text-gray-400 mb-2">
+          Select Token:
+        </label>
+        <select
+          className="w-full p-3 bg-gray-700 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none cursor-pointer"
+          value={tokenOut}
+          onChange={(e) => setTokenOut(e.target.value)}
+        >
+          <option value="USDC">USD Coin (USDC)</option>
+        </select>
+      </div>
+
+      <div className="max-w-md mx-auto mt-4 p-4 flex flex-col items-center">
         <button
           className="w-1/2 py-3 bg-pink-600 hover:bg-pink-500 text-white font-medium rounded-lg"
           onClick={onConnectClick}
